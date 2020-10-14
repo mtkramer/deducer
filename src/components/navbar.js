b7b68efc4c6b3b1a87311abcac6f539d1335788e
mtkramer <kramerm1@mail.usf.edu>
2020-10-14 15:34:33 -0400

    Cleanup, added wireframe, created navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <h1>Navbar Component</h1>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;

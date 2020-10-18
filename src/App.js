fb11da2a5c5038dba68f6715ea7fe5f1222011b9
mtkramer <kramerm1@mail.usf.edu>
2020-10-18 14:59:34 -0400

    +About component and made navbar hide on scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from "./components/about";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,17 +3,22 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav>
-                <h1>(Navbar Component)</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>Login</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                </ul>
+            <nav id="navbar">
+                <a className="navButton" href="#home">Home</a>
+                <a className="navButton" href="#login">Login</a>
+                <a className="navButton" href="#about">About</a>
+                <a className="navButton" href="#contact">Contact us</a>
             </nav>
         );
     }
 }
 
 export default Navbar;
+
+function $(x) { return document.getElementById(x); }
+var pastOffset = window.pageYOffset;
+window.onscroll = function() {
+    var currentOffset = window.pageYOffset;
+    pastOffset > currentOffset ? $("navbar").style.left = "0" : $("navbar").style.left = "-110%";
+    pastOffset = currentOffset;
+};

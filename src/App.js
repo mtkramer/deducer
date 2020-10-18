be29f8757e892f076f847b4bc88031145d1c3141
mtkramer <kramerm1@mail.usf.edu>
2020-10-18 17:00:53 -0400

    +Samples component for provided images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from "./components/about";
 import Navbar from "./components/navbar";
+import Samples from "./components/samples";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Samples />
       <About />
     </div>
   );

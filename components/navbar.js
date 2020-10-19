35a929a3e4673dad56202ffa2a094cc93bf8c3d0
mtkramer <kramerm1@mail.usf.edu>
2020-10-19 17:00:38 -0400

    Converted to React Native app

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav id="navbar">
+                <a className="navButton" href="#home">Home</a>
+                <a className="navButton" href="#login">Login</a>
+                <a className="navButton" href="#about">About</a>
+                <a className="navButton" href="#contact">Contact us</a>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
+
+// Helper function to hide navbar on scroll
+function $(x) { return document.getElementById(x); }
+var pastOffset = window.pageYOffset;
+window.onscroll = () => {
+    var currentOffset = window.pageYOffset;
+    pastOffset > currentOffset ? $("navbar").style.left = "0" : $("navbar").style.left = "-110%";
+    pastOffset = currentOffset;
+};
---
+++ components/samples.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Samples extends Component {
+    render() {
+        return (
+            <span>
+                <img alt="sample 1, " src="/assets/machu-picchu-moray.jpg"></img>
+                <img alt="sample 2, "></img>
+                <img alt="sample 3, "></img>
+                <img alt="sample 4, "></img>
+            </span>
+        );
+    }
+}
+
+export default Samples;

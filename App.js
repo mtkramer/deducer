8860c1dfc6cddf52154952fb15aa4e08eb8e2788
mtkramer <kramerm1@mail.usf.edu>
2020-10-20 10:42:23 -0400

    added stack navigation library

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
-import { styles } from "./styles";
-import About from "./components/about";
-import Navbar from "./components/navbar";
-import Samples from "./components/samples";
+import { styles } from './styles';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import About from './components/about';
+import Navbar from './components/navbar';
+import Samples from './components/samples';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Navbar />
-        <Samples />
-        <About />
-      </ScrollView>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <ScrollView>
+          <Navbar />
+          <Samples />
+          <About />
+        </ScrollView>
+      </View>
+    </NavigationContainer>
   );
 }
